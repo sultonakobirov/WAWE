1993a937e9490a9d0ae9e79ba5071e9063bf5bef
sultonakobirov <zafarzhanovich@gmail.com>
2023-05-01 13:15:10 +0500

    "some features added"

--- js/script.js
+++ js/script.js
@@ -7,32 +7,39 @@ let threeSpan = document.getElementsByClassName('span-cont')
 let days = document.querySelector('.days')
 let daysCont = document.querySelector('.days-cont')
 let showMoreModal = document.querySelector('.show-more')
-let modalWindow = document.querySelector('.modal-cont')
+let modalWindowCont = document.querySelector('.modal-cont')
+let modalWindow = document.querySelector('.modal-window')
 let closeModal = document.querySelector('.exit')
 let body = document.querySelector('body')
+
 let daysNum = 365
 const applicantForm = document.getElementById('submit-btn')
 hamburgerMenu.onclick = () =>{
     if (hamburgerMenu.checked === true) {
         menuCont.style.overflow = 'visible'
         menuCont.style.width = 'fit-content'
+        menuCont.style.height = 'fit-content'
         menuCont.style.position = 'absolute'
-        menuCont.style.top = '50px'
-        menuCont.style.right = '100px'
+        menuCont.style.top = '0px'
+        menuCont.style.right = '0px'
         menuCont.style.padding = '50px 100px'
         spanOne.style.rotate = '45deg'
         spanTwo.style.rotate = '-45deg'
         spanOne.style.position = 'absolute'
         spanTwo.style.position = 'absolute'
+        spanOne.style.zIndex = '9'
+        spanTwo.style.zIndex = '9'
         spanThree.style.display = 'none'
         spanOne.style.right = '5px'
         spanTwo.style.right = '5px'
         spanOne.style.top = '10px'
         spanTwo.style.top = '10px'
+        
     }else{
         menuCont.style.overflow = 'hidden'
-        menuCont.style.width = '0'
+        menuCont.style.height = 'fit-content'
         menuCont.style.position = 'unset'
+        menuCont.style.width = '0'
         menuCont.style.top = '0'
         menuCont.style.right = '0'
         menuCont.style.padding = '0'
@@ -56,22 +63,21 @@ function cycle(content, object, num) {
 }
 cycle(days, daysCont, daysNum)
 
-showMoreModal.onclick = ()=>{
-    modalWindow.style.display = 'flex'
-    body.style.oveflowY = 'hidden'
-    body.style.backgroundColor = 'black'
-    body.style.opacity = '0.1'
-    body.style.height = '100vh'
-    body.style.position = 'fixed'
-}
+
 showMoreModal.onclick = () =>{
-    modalWindow.style.display = 'flex'
+    modalWindowCont.style.display = 'flex'
     body.style.overflow = 'hidden'
+    modalWindowCont.style.overflow = 'auto'
+    modalWindowCont.style.height = '100vh'
+    modalWindowCont.style.width = '100%'
+
 
 }
 closeModal.onclick = () =>{
-    modalWindow.style.display = 'none'
     body.style.overflow = 'auto'
+    modalWindowCont.style.overflow = 'hidden'
+    modalWindowCont.style.height = '0'
+    modalWindowCont.style.width = '0'
 }
 
 
@@ -80,12 +86,12 @@ function handleFormSubmit(event) {
 }
 applicantForm.onclick = handleFormSubmit
 
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
         return '<span class="' + className + '">' + ("") + "</span>";
-      },
+        },
     },
-  });
+});
